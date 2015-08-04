holmberg556/cmdtest

commit d5f29a821c2dd3c8e23fe4c1ae28bdc7076f6e6c

ignore_file accepts shell glob (*, **)
+ test of this new behaviour

lib/cmdtest/fssnapshot.rb
@@ -56,7 +56,13 @@ module Cmdtest
     end
 
     def _ignore_file?(path)
-      @ignored_files.any? {|ignored| ignored === path }
+      @ignored_files.any? do |ignored|
+        if ignored.index("*")
+          File.fnmatch(ignored, path, File::FNM_PATHNAME)
+        else
+          ignored === path
+        end
+      end
     end
 
     def files

t/CMDTEST_ignore_file.rb
@@ -109,4 +109,26 @@ class CMDTEST_ignore_file < Cmdtest::Testcase
     end
   end
 
+  #----------------------------------------
+  # the argument to 'ignore_file' can contain shell wildcards,
+  # both * and **
+
+  def test_ignore_file_SHELL_GLOB
+    create_CMDTEST_foo [
+      "ignore_file 'bbb*'",
+      "ignore_file '**/ccc'",
+      "dir_mkdir 'dir'",
+      "",
+      "cmd 'touch.rb aaa bbb1 bbb2 ccc dir/aaa dir/bbb1 dir/bbb2 dir/ccc' do",
+      "  created_files 'aaa', 'dir/aaa', 'dir/bbb1', 'dir/bbb2'",
+      "end",
+    ]
+
+    cmd_cmdtest do
+      stdout_equal [
+        "### touch.rb aaa bbb1 bbb2 ccc dir/aaa dir/bbb1 dir/bbb2 dir/ccc",
+      ]
+    end
+  end
+
 end

commit 2e58c4a76a73e9c4dcb9144810e9c459edcca2cf

ignore_file can be a Regexp, forgotten in d5f29a8
this caused a regression in d5f29a8,
that should be fixed by the current commit.

lib/cmdtest/fssnapshot.rb
@@ -57,7 +57,7 @@ module Cmdtest
 
     def _ignore_file?(path)
       @ignored_files.any? do |ignored|
-        if ignored.index("*")
+        if String === ignored && ignored.index("*")
           File.fnmatch(ignored, path, File::FNM_PATHNAME)
         else
           ignored === path