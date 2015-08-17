2bb93b95365f31d1a28fbcef2dcba63e10ae1211
Johan Holmberg <holmberg556@gmail.com>
2015-08-17 23:37:50 +0200

    add test with no errors; test exit code too

--- t/CMDTEST_summary.rb
+++ t/CMDTEST_summary.rb
@@ -7,7 +7,7 @@ class CMDTEST_summery < Cmdtest::Testcase
 
   #-----------------------------------
 
-  def test_summary
+  def test_summary_ERRORS
 
     create_file "CMDTEST_foo.rb", [
       "class CMDTEST_foo1 < Cmdtest::Testcase",
@@ -68,6 +68,70 @@ class CMDTEST_summery < Cmdtest::Testcase
       stdout_equal /. 3 errors/
       stdout_equal /. 2 fatals/
       exit_nonzero
+    end
+  end
+
+  #-----------------------------------
+
+  def test_summary_OK
+
+    create_file "CMDTEST_foo.rb", [
+      "class CMDTEST_foo1 < Cmdtest::Testcase",
+      "  def setup",
+      "    prepend_path #{BIN.inspect}",
+      "    prepend_path #{PLATFORM_BIN.inspect}",
+      "  end",
+      "",
+
+      '  def test_foo1',
+      '    cmd "true" do',
+      '      exit_zero',
+      '    end',
+      '    cmd "true" do',
+      '      exit_zero',
+      '    end',
+      '    cmd "true" do',
+      '      exit_zero',
+      '    end',
+      '    cmd "true" do',
+      '      exit_zero',
+      '    end',
+      '  end',
+      '',
+      '  def test_foo2',
+      '    cmd "true" do',
+      '      exit_zero',
+      '    end',
+      '  end',
+      '',
+      '  def test_foo3',
+      '    cmd "true" do',
+      '      exit_zero',
+      '    end',
+      '  end',
+      '',
+      '  def test_foo4',
+      '    cmd "true" do',
+      '      exit_zero',
+      '    end',
+      '  end',
+      '',
+      '  def test_foo5',
+      '    cmd "true" do',
+      '      exit_zero',
+      '    end',
+      '  end',
+      '',
+      'end',
+    ]
+
+    cmd_cmdtest_verbose do
+      stdout_equal /. 1 test classes/
+      stdout_equal /. 5 test methods/
+      stdout_equal /. 8 commands/
+      stdout_equal /. 0 errors/
+      stdout_equal /. 0 fatals/
+      exit_zero
     end
   end
 
