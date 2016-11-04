cd699f4a8ec038c7b614ace83964d3a8ad0c5f63
Johan Holmberg <holmberg556@gmail.com>
2016-11-04 17:22:00 +0100

    1.4 -> 1.5, with 'skip_test' ...

--- bin/cmdtest.rb
+++ bin/cmdtest.rb
@@ -53,7 +53,7 @@ module Cmdtest
   GIT_DATE = '$GIT_DATE_STRING$'
   VERSION  = '$VERSION$'
 
-  SHORT_VERSION = '1.4'
+  SHORT_VERSION = '1.5'
 
   #----------------------------------------------------------------------
 
