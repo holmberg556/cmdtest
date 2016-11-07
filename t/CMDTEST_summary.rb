b63c6ee187377710cf9bd16c73235fefef10136a
Johan Holmberg <holmberg556@gmail.com>
2016-11-07 19:04:18 +0100

    add test of 'skipped' in summary

--- t/CMDTEST_summary.rb
+++ t/CMDTEST_summary.rb
@@ -58,14 +58,35 @@ class CMDTEST_summery < Cmdtest::Testcase
       '    end',
       '  end',
       '',
+      '  def test_skip_1',
+      '    skip_test "skip_1"',
+      '    never_called()',
+      '  end',
+      '',
+      '  def test_skip_2',
+      '    skip_test "skip_2"',
+      '    never_called()',
+      '  end',
+      '',
+      '  def test_skip_3',
+      '    skip_test "skip_3"',
+      '    never_called()',
+      '  end',
+      '',
+      '  def test_skip_4',
+      '    skip_test "skip_4"',
+      '    never_called()',
+      '  end',
+      '',
       'end',
     ]
 
     cmd_cmdtest_verbose do
-      stdout_equal /^--- .* test classes,.*test methods,.*commands,.*errors,.*fatals\.$/
+      stdout_equal /^--- .* test classes,.*test methods,.*commands,.*skipped,.*errors,.*fatals\.$/
       stdout_equal /. 1 test classes/
-      stdout_equal /. 5 test methods/
+      stdout_equal /. 9 test methods/
       stdout_equal /. 8 commands/
+      stdout_equal /. 4 skipped/
       stdout_equal /. 3 errors/
       stdout_equal /. 2 fatals/
       exit_nonzero
